ccfc1624e18c31a371a76eb22fca03458c61afce
nurmatov0228 <bahodirnur06@gmail.com>
2025-01-04 22:46:33 -0800

    members

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"; // Routing uchun
 import "../styles/services.scss";
 import image1 from "../img/assalam.jpg";
 import { useTranslation } from "react-i18next";
+import logo from "../img/logoads.png";
 
 const Services = () => {
   const { t } = useTranslation();
@@ -33,14 +34,12 @@ const Services = () => {
               className="business-slider__image"
             />
             <div className="business-slider__content">
-              <h3>{item.title}</h3>
               <p>{item.description}</p>
-              <button
-                className="more-button btn1"
+              <img
+                className="business-slider__logo"
                 onClick={() => handleMoreClick(item.id)}
-              >
-                {t("business.more")} {/* JSON faylidan tarjima olish */}
-              </button>
+                src={logo}
+              />
             </div>
           </div>
         ))}
